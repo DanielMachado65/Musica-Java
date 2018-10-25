0dfa211fbe82d3805770614d3a90bcfcea19e243
DanielMachado65 <38213401+DanielMachado65@users.noreply.github.com>
2018-10-24 23:48:20 -0300

    atualizadno o banco

--- config/database/database.sql
+++ config/database/database.sql
@@ -1,23 +1,9 @@
 create database music_bank;
 use music_bank;
 
-create table if not exists Album(
-	id int primary key auto_increment,
-    titulo varchar(100),
-    datalancamento date,
-    tempoTotal long,
-    generoId int,
-    foreign key (generoId) references Genero(id)
-);
-
 create table if not exists TipoMidia(
 	id int primary key auto_increment,
     midia varchar(100)
-);
-
-create table if not exists PessoaParcitipacao(
-	pessoaId int primary key,
-    participacaoId int primary key
 );
 
 create table if not exists Pessoa(
@@ -27,10 +13,35 @@ create table if not exists Pessoa(
     homepage varchar(200)
 );
 
+
+create table if not exists Album(
+	id int primary key auto_increment,
+    titulo varchar(100),
+    datalancamento date,
+    tempoTotal long,
+    generoId int,
+    foreign key (generoId) references Genero(id)
+);
+
 create table if not exists Participacao(
 	id int primary key auto_increment,
     descricao varchar(200)
 );
+
+create table if not exists Genero(
+	id int primary key auto_increment,
+    nome varchar(100)
+);
+
+create table if not exists PessoaParcitipacao(
+	pessoaId int primary key,
+    participacaoId int primary key,
+    foreign key (pessoaId) references Pessoa(id),
+    foreign key (participacaoId) references Participacao(id)
+);
+
+
+
 
 create table if not exists PessoPartipaco(
 	pessoaId int primary key,
@@ -52,10 +63,6 @@ create table if not exists Musica(
     foreign key (generoId) references Genero(id)
 );
 
-create table if not exists Genero(
-	id int primary key auto_increment,
-    nome varchar(100)
-);
 
 create table if not exists Faixa(
 	albumId int,
