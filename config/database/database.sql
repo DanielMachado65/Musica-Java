117635cab37450145f1dcdd8fd798a75eb1eafc8
DanielMachado65 <38213401+DanielMachado65@users.noreply.github.com>
2018-10-25 00:14:19 -0300

    primeira parte do banco
    Ainda falta na realidade verificar com a associação se vai virar um tipo tabela ou um tipo de código dentro do java

--- config/database/database.sql
+++ config/database/database.sql
@@ -1,75 +1,74 @@
 create database music_bank;
 use music_bank;
 
+-- Genero da Música
+create table if not exists Genero(
+  id int primary key auto_increment,
+  nome varchar(100) not null
+);
+
+-- TipoMdia onde que foi lançada
 create table if not exists TipoMidia(
 	id int primary key auto_increment,
-    midia varchar(100)
+  midia varchar(100)
 );
 
+-- Pessoa do tipo usuário
 create table if not exists Pessoa(
 	id int primary key auto_increment,
-    nome varchar(50),
-    nascimento date,
-    homepage varchar(200)
+  nome varchar(50),
+  nascimento date,
+  homepage varchar(200)
 );
 
-
-create table if not exists Album(
-	id int primary key auto_increment,
-    titulo varchar(100),
-    datalancamento date,
-    tempoTotal long,
-    generoId int,
-    foreign key (generoId) references Genero(id)
-);
-
+-- Participacao da Pessoa
 create table if not exists Participacao(
 	id int primary key auto_increment,
     descricao varchar(200)
 );
 
-create table if not exists Genero(
+-- Relacionamento NxN para Pessoa e Participacao
+create table if not exists PessoaPartipacao(
+  pessoaId int primary key,
+  participacaoId int primary key,
+  foreign key (pessoaId) references Pessoa(id),
+  foreign key (particapacaoId) references Participacao(id)
+);
+
+-- Album
+create table if not exists Album(
 	id int primary key auto_increment,
-    nome varchar(100)
+  titulo varchar(100),
+  datalancamento date,
+  tempoTotal long,
+  tipoMidiaId int,
+  generoId int,
+  foreign key (generoId) references Genero(id),
+  foreign key (tipoMidiaId) references TipoMdia(id)
 );
 
-create table if not exists PessoaParcitipacao(
-	pessoaId int primary key,
-    participacaoId int primary key,
-    foreign key (pessoaId) references Pessoa(id),
-    foreign key (participacaoId) references Participacao(id)
-);
-
-
-
-
-create table if not exists PessoPartipaco(
-	pessoaId int primary key,
-    participacaoId int primary key,
-    foreign key (pessoaId) references Pessoa(id),
-    foreign key (particapacaoId) references Participacao(id)
-);
-
+-- Musica
 create table if not exists Musica(
 	id int primary key auto_increment,
-    titulo varchar(50),
-    letra varchar(1000),
-    dataComposicao date,
-    genero int,
-    tag varchar(150),
-    link varchar(200),
-    generoId int,
-    rankMusica int,
-    foreign key (generoId) references Genero(id)
+  titulo varchar(50),
+  letra varchar(1000),
+  dataComposicao date,
+  tag varchar(150),
+  link varchar(200),
+  generoId int,
+  rankMusica int,
+  foreign key (generoId) references Genero(id)
 );
 
-
+-- Tabela mais dificil
 create table if not exists Faixa(
+  ordem int,
 	albumId int,
-    ordem int,
-    musicaId int,
-    generoId int,
-    duracao int,
-    foreign key (musicaId) references Musica(id),
-    foreign key (generoId) references Genero(id)
+  musicaId int,
+  generoId int,
+  duracao int,
+  foreign key (musicaId) references Musica(id),
+  foreign key (generoId) references Genero(id),
+  foreign key (albumId) references Album(id),
+  primary key (musicaId, generoId)
 );
