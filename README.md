f54dfc83de3484afa55597b322b9efcc878b3db5
DanielMachado65 <38213401+DanielMachado65@users.noreply.github.com>
2018-10-24 23:45:11 -0300

    atualizando os markdown
    Descrição mais completa

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # music_java
+
+> Trabalho com o desenvolvimento em uma aplicação Swing (**Java**) que faz o catalogo de músicas. Trabalho desenvolvido em _2018_
+
+### Requesitos
+
+1. Java
+2. Swing
+
+### Relacionamentos
+
+Essa seção para conseguir identificar o tipo de relacionamentos diferentes.
+
+```
+Faixa - 1xN - Album
+```
+
+#### O que foi requerido:
+
+```
+
+Senhores, o trabalho terá peso 20.
+
+O sistema deverá ser desenvolvido em Swing com um banco de dados embarcado (H2, JavaDB etc).
+
+A camada de persistência poderá ser realizada segundo o modelo DAO ou JPA.
+
+Foi solicitado para desenvolverem primeiro as interfaces.
+```
+
+### Interfaces
+
+![interface_inicial](inicio.png)
--- config/database/database.sql
+++ config/database/database.sql
@@ -1,11 +1,68 @@
 create database music_bank;
 use music_bank;
 
-create table if not exists album();
-create table if not exists faixa();
-create table if not exists genero();
-create table if not exists musica();
-create table if not exists participacao();
-create table if not exists pessoa();
-create table if not exists pessoaParticipacao();
-create table if not exists tipoMidia();
+create table if not exists Album(
+	id int primary key auto_increment,
+    titulo varchar(100),
+    datalancamento date,
+    tempoTotal long,
+    generoId int,
+    foreign key (generoId) references Genero(id)
+);
+
+create table if not exists TipoMidia(
+	id int primary key auto_increment,
+    midia varchar(100)
+);
+
+create table if not exists PessoaParcitipacao(
+	pessoaId int primary key,
+    participacaoId int primary key
+);
+
+create table if not exists Pessoa(
+	id int primary key auto_increment,
+    nome varchar(50),
+    nascimento date,
+    homepage varchar(200)
+);
+
+create table if not exists Participacao(
+	id int primary key auto_increment,
+    descricao varchar(200)
+);
+
+create table if not exists PessoPartipaco(
+	pessoaId int primary key,
+    participacaoId int primary key,
+    foreign key (pessoaId) references Pessoa(id),
+    foreign key (particapacaoId) references Participacao(id)
+);
+
+create table if not exists Musica(
+	id int primary key auto_increment,
+    titulo varchar(50),
+    letra varchar(1000),
+    dataComposicao date,
+    genero int,
+    tag varchar(150),
+    link varchar(200),
+    generoId int,
+    rankMusica int,
+    foreign key (generoId) references Genero(id)
+);
+
+create table if not exists Genero(
+	id int primary key auto_increment,
+    nome varchar(100)
+);
+
+create table if not exists Faixa(
+	albumId int,
+    ordem int,
+    musicaId int,
+    generoId int,
+    duracao int,
+    foreign key (musicaId) references Musica(id),
+    foreign key (generoId) references Genero(id)
+);
